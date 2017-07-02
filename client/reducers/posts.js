7f0706f6dfd9eff82dc45d636663e88662bd1bfa
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2017-07-02 17:00:25 -0300

    Add photos, comments, some actions and reducers

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,17 @@
 const posts = (state= [], action) => {
     console.log(state);
-    return state;
+    switch(action.type) {
+        case "INCREMENT_LIKES":
+            console.log("incrementing likes");
+            const i = action.index;
+            return [
+                ...state.slice(0, i), //before the one we are updating
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1) //after the one we are updating
+            ]
+        default:
+            return state;
+    }
 };
 
 export default posts;
